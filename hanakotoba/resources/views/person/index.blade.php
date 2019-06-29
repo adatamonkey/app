c595413139ba749ab3025cbd4b3b4c287a950de3
adatamonkey <adatamonkey@gmail.com>
2019-06-29 18:16:42 +0900

    モデルのリレーション

--- hanakotoba/resources/views/person/index.blade.php
+++ hanakotoba/resources/views/person/index.blade.php
@@ -6,18 +6,39 @@
   @parent
   インデックスページ
 @endsection
+
 @section('content')
   <table>
     <tr>
-      <th>Data</th>
+      <th>Data</th><th>Board</th>
     </tr>
-    @foreach($items as $item)
+    @foreach($hasItems as $item)
       <tr>
         <td>{{$item->getData()}}</td>
+        <td>
+            <table>
+              @foreach($item->boards as $obj)
+                <tr><td>{{$obj->getData()}}</td></tr>
+              @endforeach
+            </table>
+        </td>
       </tr>
     @endforeach
   </table>
+  <div>
+    <table>
+      <tr>
+        <th><th>Person</th></th>
+      </tr>
+      @foreach($noItems as $item)
+      <tr>
+        <th><td>{{$item->getData()}}</td></th>
+      </tr>
+      @endforeach
+    </table>
+  </div>
 @endsection
+
 @section('footer')
 copyright 2019 yamada.
 @endsection
